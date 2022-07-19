6ed737d84b6558edb66f7ec5945a6af6e68aba72
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-19 15:01:30 +0500

    task 1 updated

--- Tasks/evenNumberGenerator.js
+++ Tasks/evenNumberGenerator.js
@@ -1,9 +1,7 @@
-const limit = 10;
-
-function evenChallenge(limit) {
-  for (let i = 0; i <= limit; i += 2) console.log(i);
+function getEvenNumbers(limit) {
+  const result  = []
+  for (let i = 0; i <= limit; i += 2) result.push(i);
+  return result;
 }
 
-evenChallenge(limit);
-
-module.exports = evenChallenge
+module.exports = getEvenNumbers
