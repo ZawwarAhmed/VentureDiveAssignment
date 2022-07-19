1d7bea6f7be94e412b41b349f176888200fa1273
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-19 15:22:46 +0500

    test for task 2 updated

--- Tests/BMICalculator.test.js
+++ Tests/BMICalculator.test.js
@@ -1,9 +1,31 @@
-const BMI = require("../Tasks/BMICalculator");
+const getBMI = require("../Tasks/BMICalculator");
 
-it("If the weight is 50 and the height is 3, then the program should print 5.556.", () => {
-    console.log = jest.fn();
-    const weight = 50;
-    const height = 3;
-    BMI(weight, height);
-    expect(console.log.mock.calls[0][0]).toBe("5.556");
-  });
+test("If the weight is 50 and the height is 3, the function will return 5.556.", () => {
+  const weight = 50;
+  const height = 3;
+  expect(getBMI(weight, height)).toBe(5.556);
+});
+
+test("If the weight is 50 and the height is 0, the function will return height should be greater than zero..", () => {
+  const weight = 50;
+  const height = 0;
+  expect(getBMI(weight, height)).toBe("height should be greater than zero.");
+});
+
+test("If the weight is 50 and the height is -1, the function will return height should be greater than zero..", () => {
+  const weight = 50;
+  const height = -1;
+  expect(getBMI(weight, height)).toBe("height should be greater than zero.");
+});
+
+test("If the weight is 0 and the height is 3, the function will return weight should be greater than zero.", () => {
+  const weight = 0;
+  const height = 3;
+  expect(getBMI(weight, height)).toBe("weight should be greater than zero.");
+});
+
+test("If the weight is -1 and the height is 3, the function will return weight should be greater than zero.", () => {
+  const weight = -1;
+  const height = 3;
+  expect(getBMI(weight, height)).toBe("weight should be greater than zero.");
+});
