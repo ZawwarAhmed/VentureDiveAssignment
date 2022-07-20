0feb3376641eb60fe0e92c02deb11561179c622d
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-20 12:55:08 +0500

    task 2 updated

--- Tasks/Assignment 1/BMICalculator.js
+++ Tasks/Assignment 1/BMICalculator.js
@@ -1,7 +1,9 @@
 function getBMI(weight, height) {
-  if (height <= 0) return "height should be greater than zero." 
-  if (weight <= 0) return "weight should be greater than zero."
+  if (height <= 0) throw new Error("height should be greater than zero.")
+  if (weight <= 0) throw new Error("weight should be greater than zero.")
   return parseFloat((weight / (height * height)).toFixed(3));
 }
+
+console.log(getBMI(50, 3));
 
 module.exports = getBMI;
