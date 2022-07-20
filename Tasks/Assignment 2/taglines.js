2dc7914b7ec748ff2dce28024fa57f3876d8c94d
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-20 22:57:39 +0500

    task 2 completed

---
+++ Tasks/Assignment 2/taglines.js
@@ -0,0 +1,33 @@
+var taglines = [
+  {
+    name: "Spiderman",
+    tagline: "With great power, comes great responsibility.",
+  },
+  {
+    name: "Hulk",
+    tagline: "Don't make me angry. You wouldn't like me when I'm angry.",
+  },
+  {
+    name: "Batman",
+    tagline: "Error! Batman is not a superhero. Fight me!",
+  },
+  {
+    name: "Wolverine",
+    tagline: "Bub.",
+  },
+  {
+    name: "Captain America",
+    tagline: "Language!",
+  },
+];
+
+function createSuperHero(hero) {
+  const heroObject = taglines.find((tagline) => tagline.name === hero);
+  if (heroObject) return () => `${heroObject.name} | ${heroObject.tagline}`;
+  else throw new Error("hero not found.");
+}
+
+// var mySuperHero = createSuperHero("Ironman");
+// console.log(mySuperHero()); // Wolverine | Bub.
+
+module.exports = createSuperHero;
