e5e328e97c6353d1b95cf6f1ac1e15362781ef06
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-20 15:57:51 +0500

    task 3 updated

--- Tasks/Assignment 1/gradeCalculator.js
+++ Tasks/Assignment 1/gradeCalculator.js
@@ -1,27 +1,53 @@
-function getGrade(score) {
-  if (score > 100 || score < 0) return "Score should be between 0 and 100.";
-  else {
-    let grade =
-      score >= 85 && score <= 100
-        ? "A+"
-        : score >= 80 && score <= 84
-        ? "A"
-        : score >= 75 && score <= 79
-        ? "A-"
-        : score >= 70 && score <= 74
-        ? "B+"
-        : score >= 65 && score <= 69
-        ? "B"
-        : score >= 60 && score <= 64
-        ? "B-"
-        : score >= 50 && score <= 59
-        ? "C"
-        : "F";
+const gradingCriteria = [
+  {
+    grade: "A+",
+    from: 85,
+    to: 100,
+  },
+  {
+    grade: "A",
+    from: 80,
+    to: 84,
+  },
+  {
+    grade: "A-",
+    from: 75,
+    to: 79,
+  },
+  {
+    grade: "B+",
+    from: 70,
+    to: 74,
+  },
+  {
+    grade: "B",
+    from: 65,
+    to: 69,
+  },
+  {
+    grade: "B-",
+    from: 60,
+    to: 64,
+  },
+  {
+    grade: "C",
+    from: 50,
+    to: 59,
+  },
+  {
+    grade: "F",
+    from: 0,
+    to: 50,
+  },
+];
 
-    return grade;
-  }
+function getGrade(score) {
+    let result = gradingCriteria.find(item => score >= item.from && score <= item.to);
+    if (result !== undefined) 
+    return result.grade;
+    else 
+    throw new Error("Score should be between 0 and 100.")
+  
 }
 
-console.log(getGrade(76));
-
-module.exports = getGrade
+module.exports = getGrade;
