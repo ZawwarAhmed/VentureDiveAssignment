11b827e20618d784a4f8c79552b60527041c88e1
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-20 20:53:53 +0500

    task 1 updated (removed testing code)

--- Tasks/Assignment 2/closure.js
+++ Tasks/Assignment 2/closure.js
@@ -2,13 +2,4 @@ function createBase(baseNumber) {
     return (num) => baseNumber + num
 }
 
-var addFive = createBase(5);
-addFive(10); // returns 15
-addFive(21); // returns 26
-
-const addTen = createBase(10);
-
-console.log("🚀 ~ file: createBase.js ~ line 11 ~ addTen(23)", addTen(23))
-addTen(20)
-
 module.exports = createBase;
