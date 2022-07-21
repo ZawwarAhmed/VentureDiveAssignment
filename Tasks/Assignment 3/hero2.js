3ea5d316da94ebaf92fef2b305ed1eafa5e68c0d
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-21 15:05:34 +0500

    task 2 updated

---
+++ Tasks/Assignment 3/hero2.js
@@ -0,0 +1,13 @@
+function Hero(name, occupation) {
+  this.name = name;
+  this.occupation = occupation;
+}
+
+Hero.prototype.whoAreYou = function() {
+    return `My name is ${this.name} and I am a ${this.occupation}.`;
+};
+
+const hero1 = new Hero("Michaelangello", "Ninja");
+console.log(hero1.whoAreYou())
+
+module.exports = Hero;
